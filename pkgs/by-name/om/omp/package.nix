c33ed6bb0670bc1a9ade972b50b971b98d3ba144
Lunitrixx <lntrx@lunitrixx.com>
2026-08-15 19:56:43 +0200

    chore(omp): bump to 17.3.4

--- pkgs/by-name/om/omp/package.nix
+++ pkgs/by-name/om/omp/package.nix
@@ -7,11 +7,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "omp";
-  version = "17.2.10";
+  version = "17.3.4";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-T+VksjSCzWJ2caJBeEJJjJey9ytfijpO+4CU5iPfejM=";
+    hash = "sha256-P85LJWKAZLDNe/vGJF7NraMxdQ7Us0Gspr0pukR4qrU=";
   };
 
   dontUnpack = true;
